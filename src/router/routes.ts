9b7c84f45fa65ac6aaabe33df8a2da45cafc7957
Gustavo de Lima <gustavo.delima@castgroup.com.br>
2024-08-19 22:14:22 -0300

    Config routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,42 +1,46 @@
-import { RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('layouts/MainLayout.vue'),
 
-    children: [
-      { path: '', component: () => import('pages/Login.vue') },
-      { path: '/login', component: () => import('pages/Login.vue') },
-      { path: '/register', component: () => import('pages/Register.vue') },
-      {
-        path: '/payments',
-        component: () => import('pages/Payments.vue'),
-        meta: { requiresAuth: true },
-        beforeEnter: (to, from, next) => {
-          const user = localStorage.getItem('user');
-          if (!user) {
-            return next({ path: '/login' });
-          }
+      children: [
+        { path: '', component: () => import('pages/Login.vue') },
+        { path: '/login', component: () => import('pages/Login.vue') },
+        { path: '/register', component: () => import('pages/Register.vue') },
+        {
+          path: '/payments',
+          component: () => import('pages/Payments.vue'),
+          meta: { requiresAuth: true },
+          beforeEnter: (to, from, next) => {
+            const user = localStorage.getItem('user');
+            if (!user) {
+              return next({ path: '/login' });
+            }
 
-          next();
+            next();
+          },
         },
-      },
-      { path: '/about', component: () => import('pages/About.vue') },
-    ],
-  },
+        { path: '/about', component: () => import('pages/About.vue') },
+      ],
+    },
 
-  {
-    path: '/login',
-    component: () => import('pages/Login.vue'),
-  },
+    {
+      path: '/login',
+      component: () => import('pages/Login.vue'),
+    },
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/NotFound.vue'),
-  },
-];
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+      path: '/:catchAll(.*)*',
+      component: () => import('pages/NotFound.vue'),
+    },
+  ],
+});
 
-export default routes;
+
+export default router;
